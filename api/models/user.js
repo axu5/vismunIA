6a4180283ec3a9c769c11323c73e27d6f22c6a79
axu <crystalcabinss@gmail.com>
2022-09-15 12:14:55 +0200

    added documentation

--- api/models/user.js
+++ api/models/user.js
@@ -3,10 +3,15 @@ const Model = require("../model");
 const { v4: uuid } = require("uuid");
 
 class User extends Model {
-    constructor(name, email) {
+    /**
+     * @param {string} name
+     * @param {any} email
+     * @param {number} graduationYear
+     */
+    constructor(name, email, graduationYear) {
         super("users");
         this.name = name;
-        this.graduationYear = undefined;
+        this.graduationYear = graduationYear;
 
         this.email = email;
         this.userId = uuid();
@@ -22,18 +27,48 @@ class User extends Model {
         this.teacher = false;
     }
 
+    /**
+     * @param {string | Buffer} plainTextPassword
+     */
     verifyPassword(plainTextPassword) {
         return bcrypt.compareSync(plainTextPassword, this.password);
     }
 
+    /**
+     * @param {number} year
+     */
+    set graduationYear(year) {
+        year = Number(year);
+        if (isNaN(year) || year < new Date().getFullYear()) {
+            throw "cannot set graduation in the past";
+        }
+        this.graduationYear = year;
+    }
+
+    /**
+     * Hash the password when set
+     * @param {string} password
+     */
     set password(password) {
         this.password = bcrypt.hashSync(password, 12);
     }
 
-    set name(name) {
-        name = name.split(" ");
-        this.firstName = name[0];
-        this.lastName = name[1];
+    /**
+     * Sets first name and last name
+     * @param {string} fullName
+     */
+    set name(fullName) {
+        const nameArray = fullName.split(" ");
+        this.firstName = nameArray[0];
+        this.lastName = nameArray[nameArray.length - 1];
+    }
+
+    /**
+     * Get the full name of the user
+     * @returns {string} Returns full name
+     */
+    get name() {
+        return `${this.firstName} ${this.lastName}`;
     }
 }
 
